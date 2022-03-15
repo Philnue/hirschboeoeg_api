219bc5c31232ad77fc3dfbf6a2099bb9159ab78d
Philnue <philnue@gmail.com>
2022-03-15 22:53:46 +0100

    fix the database

--- apicall/apitest.py
+++ apicall/apitest.py
@@ -3,7 +3,7 @@ import requests
 import csv
 
 #res = requests.get("http://192.168.178.67:7777/loadallmitglieder/")
-res = requests.get("http://192.168.178.67:7777/loadalltermine/")
+res = requests.get("http://127.0.0.1:8000/Abstimmung/getAllAbstimmungen/")
 #res = requests.get("http://192.168.178.67:7777/loadallterminabstimmung")
 #res2 = requests.get("http://192.168.178.67:7777/loadpersonbyid/3")   /addTerminAbstimmung/{termin_id},{mitglied_id},{entscheidung}
 
